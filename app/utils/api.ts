6e0fa1b8ee071c0b5eb806b45cfeee0d1d60ad3a
Minsik Son <mson8@my.bcit.ca>
2025-02-12 20:25:14 -0800

    version -2.0.8 재정비

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,12 +1,12 @@
-import OpenAI, { OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
-const openai = new OpenAIApi({
+const openai = new OpenAI({
     apiKey: process.env.NEXT_PUBLIC_SUNO_API_KEY
 });
 
 export class SunoAPI {
     static async generatePromptWithGPT(keywords: string): Promise<Response> {
-        const response = await openai.createChatCompletion({
+        const response = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [{ role: "user", content: keywords }],
         });
