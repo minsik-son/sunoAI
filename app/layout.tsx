40557232c16892c0274008f23dc3b717de8f034e
Minsik Son <mson8@my.bcit.ca>
2025-02-13 22:55:18 -0800

    2.1.3 구글애드 해드추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang="en" data-oid="g0sbygc">
             <head>
-                
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9921649727270589"
+                        crossOrigin="anonymous"></script>
             </head>
             <body className={inter.className} data-oid="tkxr-cz">
                 {children}
